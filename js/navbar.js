6581d89131ba0b989e1d95ca929399ebecea71b2
Luka Valencia <Luka>
2023-11-27 23:09:52 +0800

    [INITIAL COMMIT] appearance of main website finished

---
+++ js/navbar.js
@@ -0,0 +1,5 @@
+document.write("                <a href=\"\">Home<\/a>");
+document.write("                <a href=\"\">Art<\/a>");
+document.write("                <a href=\"\">Wiki<\/a>");
+document.write("                <a href=\"\">Extras<\/a>");
+document.write("                <a href=\"\">Credits<\/a>");
